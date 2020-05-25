903f509f291fc8c0f6717ce797796f067e603ca1
U727184 <anna.zielinska@lhsystems.com>
2020-05-25 13:31:02 +0200

    price counter using observables

--- src/app/observable-pricing/observable-pricing.component.ts
+++ src/app/observable-pricing/observable-pricing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InstrumentService } from '../services/instrument.service';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable-pricing',
@@ -8,18 +10,51 @@ import { InstrumentService } from '../services/instrument.service';
 })
 export class ObservablePricingComponent implements OnInit {
 
-  microsoftStockPrice;
-  usdPLNprice;
+  microsoftStockPrice: number;
+  openingPriceMicrosoft: Subscription;
+  openingPriceMicrosoftValue: number;
+  changeMicrosoftStockPrice: string;
+
+  usdPlnPrice: number;
+  openingPriceUsdPln: Subscription;
+  openingPriceUsdPlnValue: number;
+  changeUSDPln: string;
+
+  openingPriceUsd: number;
 
   constructor(private instrumentService: InstrumentService) { }
 
   ngOnInit() {
+
+
+    this.openingPriceMicrosoft = this.instrumentService.microsoftSubject.subscribe( price => {
+      this.openingPriceMicrosoftValue = price as number;
+      this.openingPriceMicrosoft.unsubscribe()
+    })
+
+    console.log('first', this.instrumentService.usdplnSubject.pipe(first()))
+
+    //
+    // this.openingPriceUsdPln = this.instrumentService.usdplnSubject.subscribe( price => {
+    //   this.openingPriceUsdPlnValue = price as number;
+    //   this.openingPriceUsdPln.unsubscribe();
+    // })
+
+    this.instrumentService.usdplnSubject.pipe(first()).subscribe( price => {
+      this.openingPriceUsd = price as number;
+    })
+
     this.instrumentService.microsoftSubject.subscribe(price => {
-      this.microsoftStockPrice = price;
+      this.microsoftStockPrice = price as number;
+      this.changeMicrosoftStockPrice = ((this.microsoftStockPrice - this.openingPriceMicrosoftValue)*100/this.openingPriceMicrosoftValue).toFixed(2) + '%';
     })
+
     this.instrumentService.usdplnSubject.subscribe( price => {
-      this.usdPLNprice = price
+      this.usdPlnPrice = price as number;
+      this.changeUSDPln = ((this.usdPlnPrice - this.openingPriceUsd)*100/this.openingPriceUsd).toFixed(2) + '%';
     })
+
+
   }
 
 }
